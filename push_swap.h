6ff9bb08ef4a2bac0833fec5cddc98e0336144b5
LeKoJoy <97936414+LeKoJoy@users.noreply.github.com>
2025-03-17 20:35:02 +0000

    Updated push_swap.h, .c, created operations_*.c

---
+++ push_swap.h
@@ -0,0 +1,18 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <limits.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+/*typedef struct s_list {
+    int data;
+    struct s_list *next;
+} t_list;*/
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+#endif
---
+++ utils.c
@@ -0,0 +1,72 @@
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char	*ft_substr(const char *s, unsigned start, size_t len)
+{
+	char			*new_str;
+	unsigned int	i;
+	size_t			s_len;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	if (s == NULL)
+		return (NULL);
+	if (start >= s_len)
+		return ("");
+	if (len > s_len - start)
+		len = s_len - start;
+	new_str = (char *)malloc((len + 1) * sizeof(char));
+	if (new_str == NULL)
+		return (NULL);
+	while (s[i + start] && len)
+	{
+		new_str[i] = s[i + start];
+		i++;
+		len--;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+static size_t	how_many_words(const char *s, char c)
+{
+	size_t	count;
+	size_t	in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+		{
+			count++;
+			in_word = 1;
+		}
+		else if (*s == c)
+		{
+			in_word = 0;
+		}
+		s++;
+	}
+	return (count);
+}
+
+static void	*ft_free(char **result, size_t i)
+{
+	while (i > 0)
+	{
+		free(result[--i]);
+	}
+	free(result);
+	return (NULL);
+}
